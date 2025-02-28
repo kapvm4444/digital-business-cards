8c0ff58a8ef4d0ef1d98ebaab631055595b9c3f7
Khush <kapvm4444@gmail.com>
2025-02-28 13:35:46 +0530

    mongodb connection

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
 
+//express app
 const app = express()
 
+//development tool for the request log
+app.use(morgan('dev'))
+
+//serving the static files
 app.use(express.static('public'))
 
-app.get('*', (req, res) => {
-    res.end("Hello From Digital Business Cards")
-})
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -7,20 +7,27 @@ process.on('uncaughtException', (error) => {
     process.exit(1)
 })
 
-// const mongoose = require("mongoose");
-// mongoose.connect("", {
-//     useCreateIndex: true,
-//     useNewUrlParser: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true,
-// }).then(() => {
-//     console.log("DBC Cloud Database is connected")
-// })
+const connect_url = process.env.MONGO_URL.replace('<PASSWORD>', process.env.MONGO_PASS)
+
+const mongoose = require("mongoose");
+mongoose.connect(connect_url, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("DBC Cloud Database is connected")
+})
 
 const app = require("./app")
 const port = process.env.PORT || 3000
 const server = app.listen(port, () => {
     console.log(`Server is Running on http://127.0.0.1:${port}/`);
+})
+
+process.on('unhandledRejection', (error) => {
+    console.log("UNHANDLED REJECTION (RUNTIME ERROR) 💥");
+    console.log(`${error.name} => ${error.message}`);
 })
 
 
