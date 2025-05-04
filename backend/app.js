87211e7f290644f31f893a698fb3df9860134fd1
Khush <kapvm4444@gmail.com>
2025-05-04 12:40:39 +0530

    security middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,60 @@
 const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const hpp = require('hpp');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+const compression = require('compression');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const userRouter = require('./routers/userRouter');
 const cardRouter = require('./routers/cardRouter');
 const globalErrorHandler = require('./controllers/errorController');
 const { join } = require('node:path');
 const requestIp = require('request-ip');
+
 //express app
 const app = express();
 
 //using the requestIp middleware for getting the IP of user
 app.use(requestIp.mw());
 
+//cross-origin resource sharing
+app.use(cors({ origin: '*' }));
+
+//helmet - it changes some http headers for security
+app.use(helmet());
+
+//rateLimit - limit the rate of APIs that is called per time
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request at a time, Please try again after few hours',
+});
+/*app.use('/api', limiter);*/
+
+//let express know we are using the json and set the limit of json data to 20kb
 app.use(express.json({ limit: '20kb' }));
 
+//get the data from forms
+app.use(express.urlencoded({ extended: true, limit: '20kb' }));
+
+//mongo sanitize for sql Injection type attacks
+app.use(mongoSanitize());
+
+//hpp - for http parameter pollution (Parameters with duplicate or malicious values)
+app.use(hpp());
+
+//cross-site scripting security
+app.use(xss());
+
+//for cookies
 app.use(cookieParser());
 
-// app.use(cookieParser());
+//compression for images
+app.use(compression());
 
 //setting the view engine for rendering the email templates
 app.set('view engine', 'ejs');
@@ -28,6 +65,22 @@ app.use(morgan('dev'));
 
 //serving the static files
 app.use(express.static('public'));
+
+//routes
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/cards', cardRouter);
+
+app.get('*', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message:
+      'Welcome to the CardStream APIs, this is just a Default message\n you are not accessing api right now',
+  });
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
 
 //routes
 //=>
@@ -54,18 +107,3 @@ app.use(express.static('public'));
 // /me
 // /update-me
 // /favorites
-
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/cards', cardRouter);
-
-app.get('*', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    message:
-      'Welcome to the CardStream APIs, this is just a Default message\n you are not accessing api right now',
-  });
-});
-
-app.use(globalErrorHandler);
-
-module.exports = app;
